96f8b019b150be288b4f1fdfc7f4dc2d9c19aba6
Anna Peterson <reveragb@gmail.com>
2022-09-01 07:03:21 -0600

    Update dragon code

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,14 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+  }
+}
+greet(greeting) {
+  greeting = `Hi, ${rider}!`
+}
+hungry() {
+  hungry = true
+}
 
+module.exports = Dragon;
