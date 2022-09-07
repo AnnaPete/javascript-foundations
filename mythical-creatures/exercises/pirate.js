32de8927e5594f755913431c90240fc04ae706ab
Anna Peterson <reveragb@gmail.com>
2022-09-06 19:49:17 -0600

    Finish pirate tests

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,34 @@
+class Pirate {
+    constructor(name, job = 'scallywag', cursed = false, booty = 0) {
+        this.name = name
+        this.job = job
+        this.cursed = cursed
+        this.booty = booty
+        this.callCount = 0
+    }
+
+    robShip() {
+        this.callCount += 1;
+        if (this.callCount >= 6) {
+            this.cursed = true
+            return 'ARG! I\'ve been cursed!'
+        } else {
+            this.booty += 100
+            return 'YAARRR!'
+        };
+     };
+
+    liftCurse() {
+        if (this.cursed == true && this.booty >= 500) {
+            this.booty -= 300
+            this.cursed = false
+            return 'Your curse has been lifted!'
+        } else {
+            return 'You don\'t need to lift a curse!'
+        }
+    }
+}
+
+
+
+module.exports = Pirate
---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,10 @@
+class Vampire {
+    constructor() {
+        this.name = name;
+        this.pet = pet;
+        this.thirsty = true;
+        this.ouncesDrank = 0;
+    }
+}
+
+module.exports = Vampire;
