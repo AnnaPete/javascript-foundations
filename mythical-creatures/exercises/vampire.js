eb73be9561cd9c2f3e88b3630462fe06aba6bd43
Anna Peterson <reveragb@gmail.com>
2022-09-07 20:15:04 -0600

    Finish vampire tests

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,10 +1,20 @@
 class Vampire {
-    constructor() {
+    constructor(name, pet, thirsty, ouncesDrank) {
         this.name = name;
-        this.pet = pet;
+        this.pet = pet || 'bat';
         this.thirsty = true;
         this.ouncesDrank = 0;
-    }
+    };
+     drink() {
+        this.thirsty = false;
+        if (this.ouncesDrank < 50) {
+            this.ouncesDrank += 10;
+        } else {
+            this.ouncesDrank === 50
+            return 'I\'m too full to drink anymore!'
+        };
+    };
 }
+
 
 module.exports = Vampire;
