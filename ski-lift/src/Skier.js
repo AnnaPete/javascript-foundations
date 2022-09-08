636dadc1106598a29d10287dd69adf049b0b472c
Anna Peterson <reveragb@gmail.com>
2022-09-08 10:45:53 -0600

    Finish ski lift tests

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,4 +1,27 @@
 class Skier {
+    constructor(name) {
+        this.name = name
+        this.hasLiftTicket = true;
+        this.skillLevel = 1;
+        this.nextSlope = "green circle";
+    }
+
+    takeLesson(){
+        this.skillLevel++;
+    }
+
+//green cricle = 1
+//blue square >= 2
+//black diamond >=4
+
+    pickSlope() {
+        if (this.skillLevel == 2  || this.skillLevel === 3) {
+            this.nextSlope = "blue square";
+        } 
+        else if (this.skillLevel >= 4) {
+            this.nextSlope = "black diamond";
+        }
+    }
 
 }
 
