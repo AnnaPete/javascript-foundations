b8dcc96595f842fa85ce924855ce95a5d6509bd1
Anna Peterson <reveragb@gmail.com>
2022-09-22 10:16:52 -0600

    Finish snack tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,18 @@
-class Craft {
+const Material = require("./material");
 
+class Craft {
+    constructor(craft) {
+        this.name = craft.type;
+        this.materials = craft.materials;
+        this.completed = false;
+    }
+    completeCraft() {
+        this.completed = true;
+        return "All done! It looks great!"
+    }
+    calculateProjectTotal() {
+       this.materials[0]
+    }
 }
 
 module.exports = Craft;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,29 @@
 class Snack {
-
+    constructor(name) {
+        this.type = name;
+        this.deliciousLevel = "extra";
+        this.amount = 100;
+        this.isInLunchBox = false;
+        this.cuttingItClose = false;
+    }
+    getEaten() {
+        this.amount -= 10;
+        if (this.amount > 20) {
+            this.cuttingItClose = false;
+        } else if (this.amount <= 20) {
+            this.cuttingItClose = true;
+        }
+    }
+    checkForHealthy() {
+        var name = this.type;
+        if (name.includes("fruit")) {
+            return true;
+        } else if (name.includes("Fruit")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 module.exports = Snack;
